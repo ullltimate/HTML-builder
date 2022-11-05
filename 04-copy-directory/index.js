9d8f16779b3562d2a9747501db757422b7244980
ullltimate <ullltimate.zp@gmail.com>
2022-11-05 19:02:10 +0300

    init:start 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+fs.mkdir(path.resolve('04-copy-directory', 'files-copy'),{ recursive: true }, err => {
+   if(err) throw err;
+});
+fs.readdir(path.resolve('04-copy-directory', 'files'), (err, files) => {
+    if(err) throw err;
+    for (var i=0; i<files.length; i++){
+        fs.copyFile(path.resolve('04-copy-directory', 'files', files[i]), path.resolve('04-copy-directory', 'files-copy', files[i]), err => {
+            if(err) throw err;
+         });
+    }
+    fs.readdir(path.resolve('04-copy-directory', 'files-copy'), (err, filesCopy) => {
+        if(err) throw err;
+        for (var j=0; j<filesCopy.length; j++){
+            if (files.includes(filesCopy[j]) === false){
+                fs.unlink(path.resolve('04-copy-directory', 'files-copy', filesCopy[j]), err => {
+                    if(err) throw err;
+                 });
+            }
+        }
+    })
+    console.log('Файлы успешно скопированы в новую папку');
+});
 
